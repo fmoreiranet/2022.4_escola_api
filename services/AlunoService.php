25e8663429f1382ca0ebc38c0bbfb9a89fe9fbe7
fMoreira <fabianomoreira.net@gmail.com>
2023-03-30 21:49:18 -0300

    Aula 16

--- services/AlunoService.php
+++ services/AlunoService.php
@@ -1,7 +1,22 @@
 <?php
-include("./DAO.php");
-include("../model/Aluno.php");
+include_once("DAO.php");
+// include_once("../model/Aluno.php");
 
-function add(Aluno $aluno){
-    
+class AlunoService
+{
+    function add(Aluno $aluno)
+    {
+        try {
+            $sql = "INSERT into aluno (nome, email, senha) values (:nome, :email, :senha)";
+            $dao = new DAO;
+            $conn = $dao->connect();
+            $stman = $conn->prepare($sql); //Iniciar o preparativo para o envio dos dados ao banco;
+            $stman->bindParam(":nome", $aluno->nome); //Troca dos paramentos
+            $stman->bindParam(":email", $aluno->email);
+            $stman->bindParam(":senha", $aluno->senha);
+            $stman->execute(); //Gravar os dados no banco de dados
+        } catch (Exception $e) {
+            throw new Exception("Erro ao cadastrar!" . $e->getMessage());
+        }
+    }
 }
--- services/DAO.php
+++ services/DAO.php
@@ -1,19 +1,19 @@
 <?php
 
-class DAO{
+class DAO
+{
     private $pdo;
 
-    function connect(){
-        try{
+    function connect()
+    {
+        try {
             //new PDO("mysql:host=localhost;port=3306;dbname=dbescolaweb", root, "");
-            $pdo = new PDO('mysql:host=localhost;port=3306;dbname=dbescolaweb', "root", "");
-            echo ("Conectado!");
-        }catch(PDOException $e){
+            $pdo = new PDO('mysql:host=localhost;port=3306;dbname=dbescola', "root", "");
+            //echo json_encode(array("message" => "Conectado!"));
+        } catch (PDOException $e) {
             $pdo = null;
-            echo("<b>Error: </b>". $e->getMessage() ."</br>");
+            echo json_encode(array("error" => $e->getMessage()));
         }
         return $pdo;
     }
-
-
 }
