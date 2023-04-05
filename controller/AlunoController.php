a3ec0c7f507959c28800b155340ad27b7062d103
fMoreira <fabianomoreira.net@gmail.com>
2023-04-04 21:51:04 -0300

    Controller

--- controller/AlunoController.php
+++ controller/AlunoController.php
@@ -7,6 +7,9 @@ class AlunoController
     function postAluno()
     {
         try {
+            if (!router("post", "aluno"))
+                return;
+
             //file_get_contents: Pega dados do body contidos no request
             $body = file_get_contents('php://input');
 
@@ -27,6 +30,34 @@ class AlunoController
         } catch (Exception $e) {
             http_response_code(500);
             echo json_encode(array("error" => $e->getMessage()));
+        }
+    }
+
+    function getAluno()
+    {
+        try {
+            if (!router("get", "aluno"))
+                return;
+            echo "Get Aluno<br>";
+        } catch (Exception $e) {
+        }
+    }
+    function putAluno()
+    {
+        try {
+            if (!router("put", "aluno"))
+                return;
+            echo "Update Aluno<br>";
+        } catch (Exception $e) {
+        }
+    }
+    function deleteAluno()
+    {
+        try {
+            if (!router("delete", "aluno"))
+                return;
+            echo "Delete Aluno<br>";
+        } catch (Exception $e) {
         }
     }
 }
--- index.php
+++ index.php
@@ -6,3 +6,25 @@ header("Content-type: application/json; charset=UTF-8");
 
 $alunoController = new AlunoController();
 $alunoController->postAluno();
+$alunoController->getAluno();
+$alunoController->putAluno();
+$alunoController->deleteAluno();
+
+function router($method, $endpoint)
+{
+    try {
+        $server = $_SERVER;
+        //var_dump($server);
+
+        $method_request = strtolower($server["REQUEST_METHOD"]);
+        $uri_request = strtolower(htmlspecialchars($server["REQUEST_URI"]));
+
+        $validMethod = $method_request == strtolower($method);
+        $validURI = str_contains($uri_request, strtolower(htmlspecialchars($endpoint)));
+        var_dump($validMethod && $validURI);
+        return ($validMethod && $validURI);
+    } catch (Exception $e) {
+        http_response_code(404);
+        return false;
+    }
+}
