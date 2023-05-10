f1358164183c5e2327185d1039afd214950874de
fMoreira <fabianomoreira.net@gmail.com>
2023-05-09 21:14:33 -0300

    update

--- config.php
+++ config.php
@@ -1,6 +1,14 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+ini_set('display_startup_errors', 1);
+ini_set("enable_post_data_reading", 1);
+
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
 $_ENV = parse_ini_file('.env');
+
