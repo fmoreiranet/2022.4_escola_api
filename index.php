bb691d54c6ae155d348fd415eac3d5b6e5bb9b4f
fMoreira <fabianomoreira.net@gmail.com>
2023-05-11 20:21:45 -0300

    update

--- index.php
+++ index.php
@@ -2,4 +2,12 @@
 include_once("config.php");
 include_once("router/AlunoRouter.php");
 
-header("Content-type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+if ($method == "OPTIONS") {
+    header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: *");
+    header("Access-Control-Max-Age: 3600"); //1hora == 3600 seg;
+    header("Access-Control-Allow-Credentials: true");
+}
